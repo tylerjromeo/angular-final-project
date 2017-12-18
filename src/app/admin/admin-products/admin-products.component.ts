09ca874dd3bcd834585050ba9f8e441fb82ec2cf
Tyler Romeo <tyler.romeo@cotiviti.com>
2017-12-18 11:59:04 -0500

    add filtering

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,36 @@
+import { Product } from './../../models/product';
 import { Observable } from 'rxjs/Observable';
 import { ProductService } from './../../product.service';
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../../models/product';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products$: Observable<Product[]>;
+  products: Product[];
+  filteredProducts: Product[];
+  productsSubscription: Subscription;
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.products$ = this.productService.getAllProducts();
+    this.productsSubscription = this.productService.getAllProducts().subscribe(products => {
+      this.products = products;
+      this.filteredProducts = products;
+    });
+  }
+
+  ngOnDestroy() {
+    this.productsSubscription.unsubscribe();
+  }
+
+  filter(query) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
   }
 
 }
